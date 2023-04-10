d72e08282b12d9e92a8bde23ba0ef5f364487454
Kacy Phan <kacyphan@Kacys-Mac-mini.lan>
2023-04-10 01:11:45 -0700

    update jasemine test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,62 +1,35 @@
-describe("Galaxy Shooter game", function() {
-    let game;
+describe("Alien class", function() {
 
-    beforeEach(function() {
-        game = new GalaxyShooterGame();
+    it("should fire a bullet", function() {
+      expect(alien.fireBullet).toHaveBeenCalled();
     });
-
-    describe("Initialization", function() {
-        it("should initialize with the correct settings", function() {
-            expect(game.score).toEqual(0);
-            expect(game.lives).toEqual(3);
-            expect(game.gameOver).toBe(false);
-            expect(game.paused).toBe(false);
-        });
+  
+    it("should detect collision with bullet", function() {
+      expect(collision).toBe(true);
     });
-
-    describe("Player movement", function() {
-        it("should move the player to the left when the left arrow key is pressed", function() {
-            const initialXPosition = game.player.x;
-            expect(game.player.x).toBeLessThan(initialXPosition);
-        });
-
-        it("should move the player to the right when the right arrow key is pressed", function() {
-            const initialXPosition = game.player.x;
-            expect(game.player.x).toBeGreaterThan(initialXPosition);
-        });
+  
+    it("should destroy the alien", function() {
+      expect(alienList).not.toContain(alien);
+      expect(player.destroyAlien).toHaveBeenCalled();
     });
+  });
 
-    describe("Enemy movement", function() {
-        it("should move the enemies to the right when the game is not paused", function() {
-            const initialXPositions = game.enemies.map(enemy => enemy.x);
-            game.update(); // simulate updating the game 
-            const newPositions = game.enemies.map(enemy => enemy.x);
-            expect(newPositions.every((pos, i) => pos > initialXPositions[i])).toBe(true);
-        });
-
-        it("should not move the enemies when the game is paused", function() {
-            const initialXPositions = game.enemies.map(enemy => enemy.x);
-            game.togglePause(); // simulate pausing the game 
-            game.update(); // simulate updating the game while paused 
-            const newPosition = game.enemies.map(enemy => enemy.x);
-            expect(newPosition).toEqual(initialXPositions);
-        });
+describe('Player', () => {
+  
+    it('should move the player horizontally', () => {
+      expect(player.x).toEqual(originalX + 5);
     });
-
-    describe("Collision detection", function() {
-        it("should decrease the player's lives when an enemy collides with the player", function() {
-            const initialLives = game.lives;
-            game.enemies[0].x = game.player.x; // simulate an enemy colliding with the player 
-            game.update();
-            expect(game.lives).toEqual(initialLives - 1);
-        });
-
-        it("should increase the player's score when a bullet hits an enemy", function() {
-            const initialScore = game.score;
-            game.bullets.push(new Bullet(game.player.x, game.player.y)); // simulate the player firing a bullet 
-            game.enemies[0].y = game.bullets[0].y; // simulate a bullet hitting the enemy
-            game.update(); 
-            expect(game.score).toEqual(initialScore + 10); // assuming each enemy is worth 10 points
-        });
+  
+    it('should move the player vertically', () => {
+      expect(player.y).toEqual(originalY - 5);
+    });
+  
+    it('should fire a bullet when fireBullet is called', () => {
+      expect(player.fireBullet).toHaveBeenCalled();
+      expect(player.bulletList.length).toEqual(1);
+    });
+  
+    it('should check collision with an alien', () => {
+      expect(player.checkCollisionWithAlien(alien, bullet)).toBe(true);
     });
 });
