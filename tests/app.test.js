f6e88645ebfa2c3760cbe7487f46ae3757797a4d
Kacy Phan <kacyphan@Kacys-Mac-mini.lan>
2023-04-10 01:22:39 -0700

    update previous jasmine test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,35 +1,46 @@
-describe("Alien class", function() {
-
-    it("should fire a bullet", function() {
-      expect(alien.fireBullet).toHaveBeenCalled();
-    });
-  
-    it("should detect collision with bullet", function() {
-      expect(collision).toBe(true);
-    });
-  
-    it("should destroy the alien", function() {
-      expect(alienList).not.toContain(alien);
-      expect(player.destroyAlien).toHaveBeenCalled();
-    });
+describe("Player movement and shooting", function () {
+  it("should move the player up when the up arrow key or 'w' is pressed", function () {
+    const initialYPosition = canvas.player.y;
+    triggerKeyDownEvent(canvas, "w");
+    expect(canvas.player.y).toBeLessThan(initialYPosition);
   });
 
-describe('Player', () => {
-  
-    it('should move the player horizontally', () => {
-      expect(player.x).toEqual(originalX + 5);
-    });
-  
-    it('should move the player vertically', () => {
-      expect(player.y).toEqual(originalY - 5);
-    });
-  
-    it('should fire a bullet when fireBullet is called', () => {
-      expect(player.fireBullet).toHaveBeenCalled();
-      expect(player.bulletList.length).toEqual(1);
-    });
-  
-    it('should check collision with an alien', () => {
-      expect(player.checkCollisionWithAlien(alien, bullet)).toBe(true);
-    });
+  it("should move the player down when the down arrow key or 's' is pressed", function () {
+    const initialYPosition = canvas.player.y;
+    triggerKeyDownEvent(canvas, "s");
+    expect(canvas.player.y).toBeGreaterThan(initialYPosition);
+  });
+
+  it("should move the player to the left when the left arrow key or 'a' is pressed", function () {
+    const initialXPosition = canvas.player.x;
+    expect(canvas.player.x).toBeLessThan(initialXPosition);
+  });
+
+  it("should move the player to the right when the right arrow key or 'd' is pressed", function () {
+    const initialXPosition = canvas.player.x;
+    expect(canvas.player.x).toBeGreaterThan(initialXPosition);
+  });
+
+  it("should create a new bullet when the space bar is pressed", function () {
+    const initialBulletCount = canvas.player.bulletList.length;
+    triggerKeyDownEvent(canvas, " ");
+    expect(canvas.player.bulletList.length).toBeGreaterThan(initialBulletCount);
+  });
+});
+
+describe("Collision detection", function () {
+  it("should decrease the player's lives when an enemy collides with the player", function () {
+    const initialLives = canvas.lives;
+    canvas.enemies[0].x = canvas.player.x; // simulate an enemy colliding with the player 
+    canvas.update();
+    expect(canvas.lives).toEqual(initialLives - 1);
+  });
+
+  it("should increase the player's score when a bullet hits an enemy", function () {
+    const initialScore = canvas.score;
+    canvas.bullets.push(new Bullet(canvas.player.x, canvas.player.y)); // simulate the player firing a bullet 
+    canvas.enemies[0].y = canvas.bullets[0].y; // simulate a bullet hitting the enemy
+    canvas.update();
+    expect(canvas.score).toEqual(initialScore + 10); // assuming each enemy is worth 10 points
+  });
 });
